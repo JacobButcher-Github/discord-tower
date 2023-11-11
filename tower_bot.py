8b5508eaa90db20c240ca13ef4169838734133f0
Vincent Do <thevinnycast@gmail.com>
2023-11-11 15:59:25 -0800

    on_message architecture

--- tower_bot.py
+++ tower_bot.py
@@ -7,10 +7,19 @@ class TowerClient(Client):
     
 
     async def on_message(self, message):
-        print(f'{message.author}: {message.content}')
-
         if message.author == self.user:
             return
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+        txt = message.content.lower()
+
+        if txt.startswith('.tower'):
+            args = txt.split()
+            res = 'from tower?'
+
+            if len(args) > 1:
+                match args[1]:
+                    case "crit": # .tower crit [chance] [damage of move] [#of times used]
+                        if len(args) == 5:
+                            res = 'valid crit'
+            
+            await message.channel.send(res)
