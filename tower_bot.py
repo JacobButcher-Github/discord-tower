74f32fb325314ffc8494d5f6e12d1f7f0f37bded
JacobButcher-Github <122327437+JacobButcher-Github@users.noreply.github.com>
2023-11-11 18:59:22 -0600

    Help done

--- tower_bot.py
+++ tower_bot.py
@@ -26,25 +26,34 @@ class TowerClient(Client):
             
             await message.channel.send(res)
     
-    def help_text(self, args):
+    def help_text(self):
         res = ".tower stats set [atk][hp][spd][shi][???] -> Sets current boss to these stats\n" +\
               ".tower stats -> return stats of current boss\n\n" +\
-              +\
-              ".tower hp set -> Sets current boss to this hp\n" +\
+              ".tower hp set [Number]-> Sets current boss to this hp\n" +\
               ".tower hp add [Number] -> Adds number to  current boss hp\n" +\
               ".tower hp sub [Number] -> Subtracts number from current boss hp\n" +\
               ".tower hp -> Prints current boss hp\n\n" +\
-              +\
               ".tower batcon set [String]-> Sets current battle condition\n" +\
-              ".tower bacon -> prints current battle condition\n" +\
-              +\
+              ".tower batcon -> prints current battle condition\n" +\
               ".tower density rules -> prints density rules from TCR (my beloved)\n" +\
               ".tower density set [Number] -> Sets current density to number\n" +\
               ".tower density add [Number] -> Adds number to current density\n" +\
               ".tower density sub [Number] -> Subtracts number from current density\n" +\
-              ".tower density -> Displays current Shinsu Density" +\
-              +\
-              ""
+              ".tower density -> Displays current Shinsu Density\n\n" +\
+              ".tower turn set [Number] -> Set current turn to number\n" +\
+              ".tower turn add [Number] -> Adds number to current turn\n" +\
+              ".tower turn sub [Number] -> Subtracts number from current turn\n" +\
+              ".tower turn -> Displays current turn\n\n" +\
+              ".tower caco set [Number] -> Set current caco atk value to number\n" +\
+              ".tower caco add [Number] -> Add number to current caco atk value\n" +\
+              ".tower caco sub [Number] -> Subtract number from current caco atk value\n" +\
+              ".tower caco flag [Number] -> Returns flag buff given\n" +\
+              ".tower crit [chance] [damage of move] [# of times used] -> Calculates the damage complete with crit\n\n" +\
+              ".tower initiative add [Name] [Priority Speed] -> Prepares person in the queue\n" +\
+              ".tower initiative update [Name] [New Priority Speed] -> Updates person in queue\n" +\
+              ".tower initiative next -> Gives next person in the priority queue\n" +\
+              ".tower initiative remove [Name] -> Removes a person from queue (If ko'd, for instance)"
+        return res
 
 
     def calc_crit(self, args):
