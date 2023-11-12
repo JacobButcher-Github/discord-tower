0712a253d491fb0bb1be2001e4a08b3f031900ed
Vincent Do <thevinnycast@gmail.com>
2023-11-11 17:58:17 -0800

    Density command implemented

--- tower_bot.py
+++ tower_bot.py
@@ -7,12 +7,11 @@ class TowerClient(Client):
         print(f'Logged in as {self.user}')
 
         # State variables
-        self.boss_stats = ""
+        self.boss_stats = ''
         self.boss_hp = 0
-
-        self.batcon = ""
+        self.batcon = ''
         self.density = 0
-
+        self.turn = 0
         self.caco = 0
     
 
@@ -39,6 +38,12 @@ class TowerClient(Client):
                     
                     case 'batcon':
                         res = self.batcon_handler(args)
+                    
+                    case 'density':
+                        res = self.density_handler(args)
+                    
+                    case 'turn':
+                        res = self.turn_handler(args)
 
                     case 'crit': 
                         res = self.calc_crit(args)
@@ -57,22 +62,23 @@ class TowerClient(Client):
             ".tower hp -> Prints current boss hp\n\n" +
 
             ".tower batcon set [String]-> Sets current battle condition\n" +
-            ".tower batcon -> prints current battle condition\n" +
+            ".tower batcon -> prints current battle condition\n\n" +
 
             ".tower density rules -> prints density rules from TCR (my beloved)\n" +
             ".tower density set [Number] -> Sets current density to number\n" +
             ".tower density add [Number] -> Adds number to current density\n" +
             ".tower density sub [Number] -> Subtracts number from current density\n" +
-            ".tower density -> Displays current Shinsu Density" +
+            ".tower density -> Displays current Shinsu Density\n\n" +
 
             ".tower turn set [Number] -> Set current turn to number\n" +
             ".tower turn add [Number] -> Adds number to current turn\n" +
             ".tower turn sub [Number] -> Subtracts number from current turn\n" +
             ".tower turn -> Displays current turn\n\n" +
+
             ".tower caco set [Number] -> Set current caco atk value to number\n" +
             ".tower caco add [Number] -> Add number to current caco atk value\n" +
             ".tower caco sub [Number] -> Subtract number from current caco atk value\n" +
-            ".tower caco flag [Number] -> Returns flag buff given\n" +
+            ".tower caco flag [Number] -> Returns flag buff given\n\n" +
 
             ".tower crit [chance] [damage of move] [# of times used] -> Calculates the damage complete with crit\n\n" +
 
@@ -86,15 +92,15 @@ class TowerClient(Client):
 
 
     def stats(self, args):
-        res = "invalid stats"
+        res = 'invalid stats'
 
         if len(args) == 2:
             if self.boss_stats:
                 res = self.boss_stats
             else:
-                res = "boss stats not set"
+                res = 'boss stats not set'
         elif len(args) in [7, 8]:
-            if args[2] == "set":
+            if args[2] == 'set':
                 try:
                     atk = int(args[3])
                     hp = int(args[4])
@@ -108,7 +114,7 @@ class TowerClient(Client):
                         fifth = int(args[7])
                         self.boss_stats += f'/{fifth}???'
                     
-                    res = "stats set"
+                    res = 'stats set'
                 except:
                     pass
 
@@ -136,6 +142,72 @@ class TowerClient(Client):
                 res = f"Boss Hp: {self.boss_hp}"
         
         return res
+
+
+    def density_handler(self, args):
+        res = 'invalid density'
+
+        match len(args):
+            case 2:
+                if self.density:
+                    res = self.density
+                else:
+                    res = 'density not set'
+            
+            case 3:
+                if args[2] == 'rules':
+                    res = (
+                        'In certain areas within the Tower, characters may encounter areas with high shinsu density. ' +
+                        'These areas become more and more common the further up the Tower a character goes. ' +
+                        'When shinsu density reaches certain levels, various effects will be applied to all entities within that area. ' +
+                        'All effects of lower levels of shinsu density are also applied at a higher level of shinsu density. ' +
+                        'At the end of each turn, if a character in an area with high shinsu density has lost shinsu, ' +
+                        'they can absorb 10 shinsu from the area and lower the level of shinsu density by 1, then that character regains 10 shinsu.\n\n' +
+
+                        'Characters which have a skill named "Shinsu Resistance" are unaffected by the effects of shinsu density at levels equal ' +
+                        'to or less than their shinsu resistance level.\n\n' +
+
+                        'Level 5 – Characters lose hp equal to the shinsu density level at the end of each turn\n' +
+                        'Level 10 – Weapons that are not needles, swords, spears, and hooks deal halved damage\n' +
+                        'Level 15 – Characters lose the ability to take positions\n' +
+                        'Level 20 – Magical skills are treated as though 10 less shinsu was used on them\n' +
+                        'Level 25 – Characters lose attack and speed equal to the shinsu density level\n' +
+                        'Level 30 – Weapons and items that are not needles, swords, spears, and hooks deal 0 damage and cease to function\n' +
+                        'Level 40 – Environmental effects are negated, magical skills are treated as though 30 less shinsu was used on them, ' +
+                        'and physical attacks and skills deal halved damage\n' +
+                        'Level 50 – All entities lose the ability to move and take action'
+                    )
+
+            case 4:
+                try:
+                    num = int(args[3])
+
+                    match args[2]:
+                        case 'set':
+                            self.density = num
+                            res = f'density set to {self.density}'
+                            
+                        case 'add':
+                            self.density += num
+                            res = f'density raised to {self.density}'
+
+                        case 'sub':
+                            self.density = max(0, self.density - num)
+                            res = f'density lowered to {self.density}'
+                except:
+                    pass
+        
+        return res
+    
+
+    """
+    ".tower turn set [Number] -> Set current turn to number\n" +
+    ".tower turn add [Number] -> Adds number to current turn\n" +
+    ".tower turn sub [Number] -> Subtracts number from current turn\n" +
+    ".tower turn -> Displays current turn\n\n" +
+    """
+    def turn_handler(args):
+        pass
 
 
     def calc_crit(self, args):
