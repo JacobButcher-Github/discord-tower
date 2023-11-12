66217072c9d63129608f8dc380701bdb970808b3
Vincent Do <thevinnycast@gmail.com>
2023-11-11 17:20:41 -0800

    Stats implemented

--- tower_bot.py
+++ tower_bot.py
@@ -42,47 +42,71 @@ class TowerClient(Client):
 
     def help_text(self):
         res = (
-                ".tower stats set [atk] [hp] [spd] [shi] [???] -> Sets current boss to these stats\n" +
-                ".tower stats -> return stats of current boss\n\n" +
+            ".tower stats set [atk] [hp] [spd] [shi] [???] -> Sets current boss to these stats\n" +
+            ".tower stats -> return stats of current boss\n\n" +
 
-                ".tower hp set [Number] -> Sets current boss to this hp\n" +
-                ".tower hp add [Number] -> Adds number to  current boss hp\n" +
-                ".tower hp sub [Number] -> Subtracts number from current boss hp\n" +
-                ".tower hp -> Prints current boss hp\n\n" +
+            ".tower hp set [Number] -> Sets current boss to this hp\n" +
+            ".tower hp add [Number] -> Adds number to  current boss hp\n" +
+            ".tower hp sub [Number] -> Subtracts number from current boss hp\n" +
+            ".tower hp -> Prints current boss hp\n\n" +
 
-                ".tower batcon set [String]-> Sets current battle condition\n" +
-                ".tower batcon -> prints current battle condition\n" +
+            ".tower batcon set [String]-> Sets current battle condition\n" +
+            ".tower batcon -> prints current battle condition\n" +
 
-                ".tower density rules -> prints density rules from TCR (my beloved)\n" +
-                ".tower density set [Number] -> Sets current density to number\n" +
-                ".tower density add [Number] -> Adds number to current density\n" +
-                ".tower density sub [Number] -> Subtracts number from current density\n" +
-                ".tower density -> Displays current Shinsu Density" +
+            ".tower density rules -> prints density rules from TCR (my beloved)\n" +
+            ".tower density set [Number] -> Sets current density to number\n" +
+            ".tower density add [Number] -> Adds number to current density\n" +
+            ".tower density sub [Number] -> Subtracts number from current density\n" +
+            ".tower density -> Displays current Shinsu Density" +
 
-                ".tower turn set [Number] -> Set current turn to number\n" +
-                ".tower turn add [Number] -> Adds number to current turn\n" +
-                ".tower turn sub [Number] -> Subtracts number from current turn\n" +
-                ".tower turn -> Displays current turn\n\n" +
-                ".tower caco set [Number] -> Set current caco atk value to number\n" +
-                ".tower caco add [Number] -> Add number to current caco atk value\n" +
-                ".tower caco sub [Number] -> Subtract number from current caco atk value\n" +
-                ".tower caco flag [Number] -> Returns flag buff given\n" +
+            ".tower turn set [Number] -> Set current turn to number\n" +
+            ".tower turn add [Number] -> Adds number to current turn\n" +
+            ".tower turn sub [Number] -> Subtracts number from current turn\n" +
+            ".tower turn -> Displays current turn\n\n" +
+            ".tower caco set [Number] -> Set current caco atk value to number\n" +
+            ".tower caco add [Number] -> Add number to current caco atk value\n" +
+            ".tower caco sub [Number] -> Subtract number from current caco atk value\n" +
+            ".tower caco flag [Number] -> Returns flag buff given\n" +
 
-                ".tower crit [chance] [damage of move] [# of times used] -> Calculates the damage complete with crit\n\n" +
+            ".tower crit [chance] [damage of move] [# of times used] -> Calculates the damage complete with crit\n\n" +
 
-                ".tower initiative add [Name] [Priority Speed] -> Prepares person in the queue\n" +
-                ".tower initiative update [Name] [New Priority Speed] -> Updates person in queue\n" +
-                ".tower initiative next -> Gives next person in the priority queue\n" +
-                ".tower initiative remove [Name] -> Removes a person from queue (If ko'd, for instance)"
+            ".tower initiative add [Name] [Priority Speed] -> Prepares person in the queue\n" +
+            ".tower initiative update [Name] [New Priority Speed] -> Updates person in queue\n" +
+            ".tower initiative next -> Gives next person in the priority queue\n" +
+            ".tower initiative remove [Name] -> Removes a person from queue (If ko'd, for instance)"
         )
 
         return res
 
 
     def stats(self, args):
-        #
+        res = "invalid stats"
 
-        return self.boss_stats
+        if len(args) == 2:
+            if self.boss_stats:
+                res = self.boss_stats
+            else:
+                res = "boss stats not set"
+        elif len(args) in [7, 8]:
+            if args[2] == "set":
+                try:
+                    atk = int(args[3])
+                    hp = int(args[4])
+                    spd = int(args[5])
+                    shi = int(args[6])
+                    
+                    self.boss_stats = f'{atk}atk/{hp}hp/{spd}spd/{shi}shi'
+                    self.boss_hp = hp
+
+                    if len(args) == 8:
+                        fifth = int(args[7])
+                        self.boss_stats += f'/{fifth}???'
+                    
+                    res = "stats set"
+                except:
+                    pass
+
+        return res
 
 
     def calc_crit(self, args):
