56b519dfeeefeb6a6160d98da8619661d8126036
Vincent Do <thevinnycast@gmail.com>
2023-11-11 16:17:57 -0800

    crit command implemented

--- tower_bot.py
+++ tower_bot.py
@@ -1,4 +1,5 @@
 from discord import *
+from random import randint
 
 
 class TowerClient(Client):
@@ -13,13 +14,38 @@ class TowerClient(Client):
         txt = message.content.lower()
 
         if txt.startswith('.tower'):
+            res = 'from tower?' # Default response
             args = txt.split()
-            res = 'from tower?'
 
             if len(args) > 1:
                 match args[1]:
-                    case "crit": # .tower crit [chance] [damage of move] [#of times used]
-                        if len(args) == 5:
-                            res = 'valid crit'
+                    case 'crit': 
+                        res = self.calc_crit(args)
             
             await message.channel.send(res)
+    
+
+    def calc_crit(self, args):
+        # .tower crit [chance] [damage of move] [# of times used]
+        res = 'invalid crit'
+
+        if len(args) == 5:
+            chance = int(args[2])
+            dmg = int(args[3])
+            times = int(args[4])
+
+            if isinstance(chance, int) and isinstance(dmg, int) and isinstance(times, int):
+                crit_dmg = dmg * times * (2 ** self.roll_success(chance))
+                res = f'crit for {crit_dmg}'
+        
+        return res
+
+
+    def roll_success(self, percent):
+        success = percent // 100
+        percent %= 100
+
+        if randint(1, 100) <= percent:
+            success += 1
+
+        return success
