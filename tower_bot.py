d60669d0dde9eed2b5bac3acbdd97d579a5a2f32
Vinny <thevinnycast@gmail.com>
2025-01-09 19:28:49 -0600

    Migrate link constants

--- tower_bot.py
+++ tower_bot.py
@@ -434,8 +434,7 @@ class TowerClient(Client):
 
 
     def tcr(self):
-        return ('<https://drmmo.proboards.com/thread/6403/tower-comprehensive-rules>\n' +
-                'https://media.discordapp.net/attachments/245589536845856777/1111426152096071760/makesweet-ghv12k.gif?ex=65a070c3&is=658dfbc3&hm=25dd2c292fae849b2a832804759a5b43aae9e016c3913829f5826de693e3dab6&')
+        return TCR
 
     def kerta(self):
-        return ("Kerta🧍‍♂️40,000 HP ❤️8000 atk💪7500 spd 🏃Unresistable 🚫 Statuses blood field 🩸30 stacks every action 🎬Reactive Grab 🤝 that heals him 💗8000 targeted AoE 🌋 hybrid ☯️Cooldown is only ☝️ turn too 🕐Instant attack and move action per 10 bleeding ‼️ Sets attack, speed, shinsu to 0️⃣ when grabbed with Honden's🧙‍♂️ Chains ⛓️ + sealed 🔒. Multi ranged 🎯grab with it too! Has alligators 🐊 he didn't even use!")
+        return KERTA
