2f63442ea5b0009b8aad9330057e7b0a81e3db46
Vinny <thevinnycast@gmail.com>
2025-05-27 20:15:05 -0500

    use constance in main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from discord import Intents
 from tower_bot import TowerClient
+import constance as fedralis
 
 
 if __name__ == '__main__':
-    token: str = open('token.txt').read()
+    token: str = open(fedralis.TOKEN_FILE).read()
 
     intents: Intents = Intents.default()
     intents.message_content = True
